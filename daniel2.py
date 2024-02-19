2d761e33f56f4bcc291bd8c0520212c70c067313
Duskalor <duskalor1@gmail.com>
2024-02-19 09:37:52 -0500

    cositas

--- daniel2.py
+++ daniel2.py
@@ -2,3 +2,6 @@ rango = range(32, 127) # rango de valores ASCII
 for valor in rango: # bucle que recorre el rango
     print(chr(valor), end=" ") # imprime el carácter correspondiente
 print() # salto de línea
+
+
+s
